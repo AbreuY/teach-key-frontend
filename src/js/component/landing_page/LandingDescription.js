d0afb5375a7a4eba41827c0ed6cd5cf37dd4b40f
Aaron Arismendi <arismendiaaron@gmail.com>
2021-11-02 18:00:55 -0400

    logo

--- src/js/component/landing_page/LandingDescription.js
+++ src/js/component/landing_page/LandingDescription.js
@@ -6,7 +6,7 @@ export const LandingDescription = () => {
 			<div className="d-flex flex-row">
 				<img
 					style={{ height: "7rem", width: "7rem" }}
-					src="https://www.kindpng.com/picc/m/379-3796905_dibujo-de-bombilla-para-colorear-illustration-hd-png.png"
+					src='logo.png'
 				/>
 				<div>
 					<h2>Learn with ease.</h2>
