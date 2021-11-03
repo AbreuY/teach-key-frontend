2438ab87be594cfed965efadcbff7c046b1fe5d8
Jesuspestana01 <jesuspestana77@gmail.com>
2021-11-03 18:33:12 -0400

    push

--- src/js/component/landing_page/LandingNavbar.js
+++ src/js/component/landing_page/LandingNavbar.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../../../img/logo.png";
+
 export const LandingNavbar = () => {
 	return (
 		<div className="container-fluid bg-light">
-			<nav className="navbar navbar-expand-lg navbar-light">
+			<nav className="navbar navbar-expand-lg navbar-light row">
 				<div className="container">
 					<Link to="/" className="navbar-brand" href="#">
 						<div className="d-flex">
-							<img style={{ height: "3.75rem", width: "6.5" }} src={Logo} />
-							<div className="h1 ms-2 align-self-center ">Teach Key</div>
-							<span className="h2 ps-8"> Nunca es tarde para aprender algo nuevo</span>
+							<img
+								className="col-2 col-md-3 col-lg-3 "
+								style={{ height: "3.75rem", width: "6.5" }}
+								src={Logo}
+							/>
+							<div className="col-2 col-md-3 col-lg-3 ">Teach Key</div>
+							<span className="col-2 col-md-3 col-lg-3 h2 ps-8">
+								{" "}
+								Nunca es tarde para aprender algo nuevo
+							</span>
 						</div>
 					</Link>
-					<button type="button" className="btn btn-primary">
+					<button type="button" className="btn btn-primary col-2 col-md-3 col-lg-3">
 						Main Page
 					</button>
 				</div>
