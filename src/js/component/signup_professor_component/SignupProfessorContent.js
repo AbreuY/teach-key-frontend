10c67cdd7e4b5f8dae72e20a3fa221802b8f7be5
Jesuspestana01 <jesuspestana77@gmail.com>
2021-11-04 18:05:54 -0400

    modifies

--- src/js/component/signup_professor_component/SignupProfessorContent.js
+++ src/js/component/signup_professor_component/SignupProfessorContent.js
@@ -1,17 +1,46 @@
 import React from "react";
+import GirlStudying from "../../../img/girlstudying.jpg";
 
 export const SignupProfessorContent = () => {
 	return (
 		<>
 			<div className="container">
 				<div className="row align-items-center">
-					<div className="col-12 col-md-6 col-sm-12 text-md-center text-sm-center text-center">
+					<div className="col-12 col-md-6 col-sm-12 col-lg-6 text-md-center text-sm-center signupBox text-center shadow-lg p-3 mb-5 bg-body rounded">
 						<div className="fs-2">Sign Up as a Professor</div>
-						<p className="text-secondary">Without paying anything!</p>
-						<input />
+						<p className="text-secondary pt-3">Without paying anything!</p>
+						<div className="row pt-3 signupInput">
+							<div className="col-12 col-md-12 col-sm-6 col-lg-6 pt-3">
+								<input className="pt-3 pt-sm-3 pt-md-2 pt-lg-0" type="text" placeholder="Username" />
+							</div>
+							<div className="col-12 col-md-12 col-sm-6 col-lg-6 pt-3">
+								<input className="pt-3 pt-sm-3 pt-md-2 pt-lg-0" type="email" placeholder="Email" />
+							</div>
+						</div>
+						<div className="row pt-0 pt-sm-0 pt-md-0 pt-lg-3 mt-4 signupInput">
+							<div className="col-12 col-md-12 col-sm-6 col-lg-6 pt-0 pt-sm-3 pt-md-0 pt-lg-3">
+								<input className="pt-3 pt-sm-3 pt-md-2 pt-lg-0" type="text" placeholder="Password" />
+							</div>
+							<div className="col-12 col-md-12 col-sm-6 col-lg-6 pt-3">
+								<input
+									className="pt-3 pt-sm-3 pt-md-2 pt-lg-0"
+									type="email"
+									placeholder="Confirm Password"
+								/>
+							</div>
+						</div>
+						<div className="row pt-0 pt-sm-0 pt-md-0 pt-lg-3 mt-4 signupInput" />
 					</div>
 					<div className="col-md-6 text-md-center text-sm-center text-center">
-						<p>LOEAFAEFAEFASFAS</p>
+						<div className="row">
+							<div className="col">
+								<img
+									className="rounded-circle m-2 shadow-lg mb-5 bg-body"
+									style={{ height: "25rem", width: "32.5rem" }}
+									src={GirlStudying}
+								/>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
--- src/js/component/signup_professor_component/SignupProfessorNavbar.js
+++ src/js/component/signup_professor_component/SignupProfessorNavbar.js
@@ -5,7 +5,7 @@ import Logo from "../../../img/logo2.png";
 export const SignupProfessorNavbar = () => {
 	return (
 		<div className="container">
-			<div className="row align-items-center m-3 mb-5">
+			<div className="row align-items-center m-3 mb-5 shadow-sm p-3 mb-5 bg-body rounded">
 				<div className="col-12 col-md-6 col-sm-12 text-md-start text-sm-center text-center">
 					<img src={Logo} style={{ height: "60px" }} />
 					<Link to="/" className="navbar-brand text-dark ms-2">
