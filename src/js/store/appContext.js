86e51e713237f3141135e4ecec20e502e633fe01
AbreuY <yolfran12@gmail.com>
2021-11-05 18:41:37 -0500

    manage section content

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -5,7 +5,20 @@ export const AppContext = createContext(undefined);
 
 const AppContextProvider = ({ children }) => {
 	const [store, setStore] = useState({
-		data: []
+		sectionInfo: [
+			{
+				imgUrl: "https://i.imgur.com/cQIzDHf.jpg",
+				title: "Yo soy el h1 de la seccion a",
+				description: "Some description",
+				orientation: "left"
+			},
+			{
+				imgUrl: "https://i.imgur.com/cQIzDHf.jpg",
+				title: "Yo soy el h1 de la seccion b",
+				description: "Some description b",
+				orientation: "right"
+			}
+		]
 	});
 
 	const actions = {};
--- src/js/views/Home.js
+++ src/js/views/Home.js
@@ -1,17 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { TopServices } from "../component/home_components/TopServices";
 import { TopProfessors } from "../component/home_components/TopProfessors";
 import { Container3 } from "../component/home_components/Container3";
 
 import { Jumbotron } from "../component/home_components/Jumbotron";
+import { AppContext } from "../store/appContext";
 
 export const Home = () => {
+	const { store, actions } = useContext(AppContext);
 	return (
 		<>
 			<Jumbotron />
 			<TopServices />
-			<Container3 />
+
+			<Container3 section={store.sectionInfo[0]} />
 			<TopProfessors />
+			<Container3 section={store.sectionInfo[1]} />
 		</>
 	);
 };
