83a274ed4ee52229968caccb24bdfe860d92cfc7
AbreuY <yolfran12@gmail.com>
2021-11-08 18:30:38 -0500

    removed duplicated container

--- src/js/component/home_components/Container3.js
+++ src/js/component/home_components/Container3.js
@@ -6,18 +6,16 @@ export const Container3 = props => {
 	return (
 		<div className="container-fluid bg-ligth-green mt-5">
 			<div className="container">
-				<div className="container">
-					<div className={props.section.orientation == "right" ? "d-flex flex-row-reverse" : "row"}>
-						<div className="col-md-6">
-							<div>
-								<img alt="info" className="img-fluid p-4 img-rounded" src={props.section.imgUrl} />
-							</div>
+				<div className={props.section.orientation == "right" ? "d-flex flex-row-reverse" : "row"}>
+					<div className="col-md-6">
+						<div>
+							<img alt="info" className="img-fluid p-4 img-rounded" src={props.section.imgUrl} />
 						</div>
-						<div className="col-md-6 col-lg-5 ml-auto d-flex align-items-center mt-4 mt-md-0">
-							<div className="text-center">
-								<h2>{props.section.title}</h2>
-								<p>{props.section.description}</p>
-							</div>
+					</div>
+					<div className="col-md-6 col-lg-5 ml-auto d-flex align-items-center mt-4 mt-md-0">
+						<div className="text-center">
+							<h2>{props.section.title}</h2>
+							<p>{props.section.description}</p>
 						</div>
 					</div>
 				</div>
