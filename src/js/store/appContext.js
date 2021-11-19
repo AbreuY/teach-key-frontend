ed7c6b26cf92e1baddd5330a56399bf0b73df9b1
Jesuspestana01 <jesuspestana77@gmail.com>
2021-11-18 20:59:06 -0400

    added filter

--- src/js/store/appContext.js
+++ src/js/store/appContext.js
@@ -79,7 +79,7 @@ const AppContextProvider = ({ children }) => {
 		setTitle: data => {
 			let title = "";
 			if (store.isFromEdit) {
-				title = store.singleService.title = data;
+				matter = store.singleService.title = data;
 			} else {
 				title = store.newService.title = data;
 			}
@@ -88,6 +88,7 @@ const AppContextProvider = ({ children }) => {
 				title
 			}));
 		},
+
 		setDescription: data => {
 			let description = "";
 			if (store.isFromEdit) {
